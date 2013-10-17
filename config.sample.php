035ccb348c43e9cd256558d3aa9f45bfff22f642
Aare Tamm” <aare.tamm@khk.ee>
2013-10-17 10:22:44 +0300

    Delete config sample .php

--- config.sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', 'blog');
-define('DEFAULT_CONTROLLER', 'welcome');
-define('BASE_URL', '/blog/');
-define('ASSETS_URL', '/blog/assets/');
