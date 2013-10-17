c65a732d5f6dcb6b247170180ad3ba8df8b5c54f
Aare Tamm” <aare.tamm@khk.ee>
2013-10-17 10:24:59 +0300

    add config sample .php

---
+++ config.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_DATABASE', 'blog');
+define('DEFAULT_CONTROLLER', 'welcome');
+define('BASE_URL', '/blog/');
+define('ASSETS_URL', '/blog/assets/');
